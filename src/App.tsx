0ad08d5fcfd89fb64794d523e2bde69dacbe6052
jeancarlos236 <jeancarlos20001220@gmail.com>
2020-01-30 23:21:18 -0400

    version 1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <p>
+         <code>Jean Carlos Muñoz</code>
+        </p>
+    </div>
+  );
+}
+
+export default App;
